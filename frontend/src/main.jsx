07b2428b1bbda2d486f9aef8647d5a30b8196c3a
CypherC <benhammouda99@gmail.com>
2024-08-21 14:42:58 +0200

    fix authcontext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { AuthContextProvider } from "/context/AuthContext.jsx";
-import { SocketContextProvider } from "/context/SocketContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext";
+import { SocketContextProvider } from "./context/SocketContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
