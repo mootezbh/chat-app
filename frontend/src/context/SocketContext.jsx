c004263610f23811ece4fc3dd3419847ed52a9ac
CypherC <benhammouda99@gmail.com>
2024-08-21 15:05:20 +0200

    socket url update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://chat-app-prod-udd9.onrender.com", {
         query: {
           userId: authUser._id,
         },
